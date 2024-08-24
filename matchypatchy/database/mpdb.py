96de3e39ba9d1c24d8aeb4ddca7d5ba5e4a73c43
tkswanson <ts9137b@gmail.com>
2024-08-23 18:27:57 -0700

    friday commit
    main page in gui, funcs to add survey

---
+++ matchypatchy/database/mpdb.py
@@ -0,0 +1,71 @@
+'''
+Class Definition for MatchyPatchyDB
+'''
+import sqlite3
+
+from . import setup
+
+class MatchyPatchyDB():
+    def __init__(self, filepath='matchypatchy.db'):
+        self.filepath = filepath
+        self.initiate = setup.setup_database(filepath)
+    
+    def validate(self):
+        db = sqlite3.connect(self.filepath)
+        cursor = db.cursor()
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+        db.close()
+        return tables
+    
+    def add_survey(self, name, year_start, year_end, region):
+        try:
+            db = sqlite3.connect(self.filepath)
+            cursor = db.cursor()
+            insert_survey = """INSERT INTO survey
+                            (name, year_start, year_end, region) 
+                            VALUES (?, ?, ?, ?);"""
+            data_tuple = (name,  year_start, year_end, region)
+            cursor.execute(insert_survey, data_tuple)
+            db.commit()
+            db.close()
+            return True
+        except sqlite3.Error as error:
+            print("Failed to insert into survey table", error)
+            if db:
+                db.close()
+            return False
+        
+    def add_site(self, name, lat, long, survey_id):
+        try:
+            db = sqlite3.connect(self.filepath)
+            cursor = db.cursor()
+            insert_survey = """INSERT INTO site
+                            (name, lat, long, survey_id) 
+                            VALUES (?, ?, ?, ?);"""
+            data_tuple = (name,  lat, long, survey_id)
+            cursor.execute(insert_survey, data_tuple)
+            db.commit()
+            db.close()
+            return True
+        except sqlite3.Error as error:
+            print("Failed to insert into survey table", error)
+            if db:
+                db.close()
+            return False
+        
+    def fetch_table(self, table):
+        db = sqlite3.connect(self.filepath)
+        cursor = db.cursor()
+        cursor.execute(f'SELECT * FROM {table};')
+        rows = cursor.fetchall()
+        db.close()
+        return rows
+    
+    def fetch_column(self, table, column):
+        db = sqlite3.connect(self.filepath)
+        cursor = db.cursor()
+        cursor.execute(f'SELECT {column} FROM {table};')
+        rows = cursor.fetchall()  # returns in tuple
+        db.close()
+        return [el[0] for el in rows]
