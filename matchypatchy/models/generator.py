2acd89820067e47b7d2f0a7d15951f156e5a9142
Kyra Swanson <t.kyraswanson@gmail.com>
2024-09-04 16:26:31 -0700

    prep for miewid

--- matchypatchy/models/generator.py
+++ matchypatchy/models/generator.py
@@ -1,22 +1,24 @@
-from PIL import Image
-import torch
-import torchvision.transforms as transforms
-import numpy as np
-
-
-
 from PIL import Image,  ImageFile
 from numpy import max
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms.functional as F
-from torchvision.transforms import (Compose, Resize, Normalize, ToTensor)
+from torchvision.transforms import (Compose, Resize, ToTensor)
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+# MIEWID takes 480x480
 
 
 
-preprocess = Compose([Resize((440, 440)),
-                      ToTensor(),
-                      Normalize(mean=[0.485, 0.456, 0.406],
-                                std=[0.229, 0.224, 0.225]),])
+class SquarePad:
+    def __call__(self, image):
+        size = image.size()
+        max_wh = max(size)
+        hp = int((max_wh - size[-1]) / 2)
+        vp = int((max_wh - size[-2]) / 2)
+        padding = (hp, vp, hp, vp)
+        return F.pad(image, padding, 0, 'constant')
 
 
 class ImageGenerator(Dataset):
@@ -25,16 +27,20 @@ class ImageGenerator(Dataset):
     ie from MegaDetector
 
     Options:
+        - file_col: column name containing full file paths
         - resize: dynamically resize images to target (square) [W,H]
     '''
-    def __init__(self, x, resize_height=440, resize_width=440):
+    def __init__(self, x, file_col='file', resize_height=480, resize_width=480):
         self.x = x
+        self.file_col = file_col
         self.resize_height = int(resize_height)
         self.resize_width = int(resize_width)
-        self.transform = transforms.Compose([Resize((self.resize_height, self.resize_width)),
-                                             ToTensor(),
-                                             Normalize(mean=[0.485, 0.456, 0.406],
-                                                       std=[0.229, 0.224, 0.225]),])
+        self.transform = Compose([
+            SquarePad(),
+            # torch.resize order is H,W
+            Resize((self.resize_height, self.resize_width)),
+            ToTensor(),
+            ])
 
     def __len__(self):
         return len(self.x)
@@ -52,27 +58,34 @@ class ImageGenerator(Dataset):
         width, height = img.size
 
         if self.crop:
-            bbox1 = self.x['bbox_x'].iloc[idx]
-            bbox2 = self.x['bbox_y'].iloc[idx]
-            bbox3 = self.x['bbox_w'].iloc[idx]
-            bbox4 = self.x['bbox_h'].iloc[idx]
+            bbox1 = self.x['bbox1'].iloc[idx]
+            bbox2 = self.x['bbox2'].iloc[idx]
+            bbox3 = self.x['bbox3'].iloc[idx]
+            bbox4 = self.x['bbox4'].iloc[idx]
 
             left = width * bbox1
             top = height * bbox2
             right = width * (bbox1 + bbox3)
             bottom = height * (bbox2 + bbox4)
 
+            left = max(0, int(left) - self.buffer)
+            top = max(0, int(top) - self.buffer)
+            right = min(width, int(right) + self.buffer)
+            bottom = min(height, int(bottom) + self.buffer)
             img = img.crop((left, top, right, bottom))
 
         img_tensor = self.transform(img)
         img.close()
 
-        return img_tensor
+        if not self.normalize:  # un-normalize
+            img_tensor = img_tensor * 255
+
+        return img_tensor, image_name
     
     
 
 
-def dataloader(manifest, batch_size=1, workers=1, resize_height=440, resize_width=440):
+def dataloader(manifest, batch_size=1, workers=1, file_col="file"):
     '''
         Loads a dataset and wraps it in a PyTorch DataLoader object.
         Always dynamically crops
@@ -90,7 +103,9 @@ def dataloader(manifest, batch_size=1, workers=1, resize_height=440, resize_widt
         Returns:
             dataloader object
     '''
-    dataset_instance = ImageGenerator(manifest, resize_height=440, resize_width=440)
+    
+    # default values file_col='file', resize=299
+    dataset_instance = ImageGenerator(manifest)
 
     dataLoader = DataLoader(
             dataset=dataset_instance,
@@ -99,21 +114,3 @@ def dataloader(manifest, batch_size=1, workers=1, resize_height=440, resize_widt
             num_workers=workers
         )
     return dataLoader
-
-
-def load_miew(file_path):
-    model = torch.load(file_path)
-    return model
-
-
-def extract_roi_embedding(model, dataloader):
-    with torch.no_grad():
-        output = model(dataloader)
-
-    print(output)
-    print(output.shape)
-    return output
-
-
-model = load_miew('/home/kyra/matchypatchy/matchypatchy/models/miew_id/miew_id_all.bin')
-extract_roi_embedding(model, dataloader)
