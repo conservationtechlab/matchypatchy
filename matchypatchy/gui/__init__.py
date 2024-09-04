eb2dd289412a44df989597380af422e3505c3924
Kyra Swanson <t.kyraswanson@gmail.com>
2024-09-04 14:02:58 -0700

    add init files

---
+++ matchypatchy/gui/__init__.py
@@ -0,0 +1,31 @@
+from matchypatchy.gui import display_base
+from matchypatchy.gui import display_compare
+from matchypatchy.gui import display_media
+from matchypatchy.gui import display_single
+from matchypatchy.gui import main_gui
+from matchypatchy.gui import popup_alert
+from matchypatchy.gui import popup_confirm
+from matchypatchy.gui import popup_site
+from matchypatchy.gui import popup_species
+from matchypatchy.gui import popup_survey
+from matchypatchy.gui import widget_image
+
+from matchypatchy.gui.display_base import (DisplayBase,)
+from matchypatchy.gui.display_compare import (DisplayCompare,)
+from matchypatchy.gui.display_media import (DisplayMedia,)
+from matchypatchy.gui.display_single import (DisplaySingle,)
+from matchypatchy.gui.main_gui import (MainWindow, main_display,)
+from matchypatchy.gui.popup_alert import (AlertPopup,)
+from matchypatchy.gui.popup_confirm import (ConfirmPopup,)
+from matchypatchy.gui.popup_site import (SiteFillPopup, SitePopup,)
+from matchypatchy.gui.popup_species import (SpeciesFillPopup, SpeciesPopup,)
+from matchypatchy.gui.popup_survey import (SurveyPopup,)
+from matchypatchy.gui.widget_image import (ImageWidget,)
+
+__all__ = ['AlertPopup', 'ConfirmPopup', 'DisplayBase', 'DisplayCompare',
+           'DisplayMedia', 'DisplaySingle', 'ImageWidget', 'MainWindow',
+           'SiteFillPopup', 'SitePopup', 'SpeciesFillPopup', 'SpeciesPopup',
+           'SurveyPopup', 'display_base', 'display_compare', 'display_media',
+           'display_single', 'main_display', 'main_gui', 'popup_alert',
+           'popup_confirm', 'popup_site', 'popup_species', 'popup_survey',
+           'widget_image']
