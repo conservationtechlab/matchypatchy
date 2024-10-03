ccdc3e8d88cc21ef4766399a023cb05f48b7e764
Kyra Swanson <t.kyraswanson@gmail.com>
2024-10-02 17:17:41 -0700

    move reid to animl

--- matchypatchy/__init__.py
+++ matchypatchy/__init__.py
@@ -1,13 +1,13 @@
 from matchypatchy import database
 from matchypatchy import gui
-from matchypatchy import models
 from matchypatchy import sqlite_vec
 from matchypatchy import test_install
 from matchypatchy import utils
 
 from matchypatchy.database import (MatchyPatchyDB, fetch_media, fetch_roi,
-                                   fetch_sites, import_csv, media, mpdb, roi,
-                                   roi_knn, setup, setup_database, site,
+                                   fetch_sites, fetch_surveys, import_csv,
+                                   media, mpdb, roi, roi_knn, setup,
+                                   setup_database, site, survey,
                                    update_roi_embedding, update_roi_iid,
                                    update_roi_viewpoint,)
 from matchypatchy.gui import (AlertPopup, ConfirmPopup, DisplayBase,
@@ -18,37 +18,21 @@ from matchypatchy.gui import (AlertPopup, ConfirmPopup, DisplayBase,
                               display_media, display_single, main_display,
                               main_gui, popup_alert, popup_confirm, popup_site,
                               popup_species, popup_survey, widget_image,)
-from matchypatchy.models import (ArcFaceLossAdaptiveMargin,
-                                 ArcFaceSubCenterDynamic, ArcMarginProduct,
-                                 ArcMarginProduct_subcenter, ElasticArcFace,
-                                 GeM, IMAGE_HEIGHT, IMAGE_WIDTH,
-                                 ImageGenerator, MiewIdNet, ViewpointModel,
-                                 dataloader, generator, heads, l2_norm, load,
-                                 miewid, predict, viewpoint,
-                                 weights_init_classifier,
-                                 weights_init_kaiming,)
 from matchypatchy.sqlite_vec import (load, loadable_path, register_numpy,
                                      serialize_float32, serialize_int8,)
 from matchypatchy.test_install import (main,)
 from matchypatchy.utils import (is_unique, swap_keyvalue,)
 
-__all__ = ['AlertPopup', 'ArcFaceLossAdaptiveMargin',
-           'ArcFaceSubCenterDynamic', 'ArcMarginProduct',
-           'ArcMarginProduct_subcenter', 'ConfirmPopup', 'DisplayBase',
-           'DisplayCompare', 'DisplayMedia', 'DisplaySingle', 'ElasticArcFace',
-           'GeM', 'IMAGE_HEIGHT', 'IMAGE_WIDTH', 'ImageGenerator',
-           'ImageWidget', 'MainWindow', 'MatchyPatchyDB', 'MiewIdNet',
-           'SiteFillPopup', 'SitePopup', 'SpeciesFillPopup', 'SpeciesPopup',
-           'SurveyPopup', 'ViewpointModel', 'database', 'dataloader',
-           'display_base', 'display_compare', 'display_media',
-           'display_single', 'fetch_media', 'fetch_roi', 'fetch_sites',
-           'generator', 'gui', 'heads', 'import_csv', 'is_unique', 'l2_norm',
-           'load', 'loadable_path', 'main', 'main_display', 'main_gui',
-           'media', 'miewid', 'models', 'mpdb', 'popup_alert', 'popup_confirm',
-           'popup_site', 'popup_species', 'popup_survey', 'predict',
-           'register_numpy', 'roi', 'roi_knn', 'serialize_float32',
-           'serialize_int8', 'setup', 'setup_database', 'site', 'sqlite_vec',
-           'swap_keyvalue', 'test_install', 'update_roi_embedding',
-           'update_roi_iid', 'update_roi_viewpoint', 'utils', 'viewpoint',
-           'weights_init_classifier', 'weights_init_kaiming', 'widget_image']
-
+__all__ = ['AlertPopup', 'ConfirmPopup', 'DisplayBase', 'DisplayCompare',
+           'DisplayMedia', 'DisplaySingle', 'ImageWidget', 'MainWindow',
+           'MatchyPatchyDB', 'SiteFillPopup', 'SitePopup', 'SpeciesFillPopup',
+           'SpeciesPopup', 'SurveyPopup', 'database', 'display_base',
+           'display_compare', 'display_media', 'display_single', 'fetch_media',
+           'fetch_roi', 'fetch_sites', 'fetch_surveys', 'gui', 'import_csv',
+           'is_unique', 'load', 'loadable_path', 'main', 'main_display',
+           'main_gui', 'media', 'mpdb', 'popup_alert', 'popup_confirm',
+           'popup_site', 'popup_species', 'popup_survey', 'register_numpy',
+           'roi', 'roi_knn', 'serialize_float32', 'serialize_int8', 'setup',
+           'setup_database', 'site', 'sqlite_vec', 'survey', 'swap_keyvalue',
+           'test_install', 'update_roi_embedding', 'update_roi_iid',
+           'update_roi_viewpoint', 'utils', 'widget_image']
