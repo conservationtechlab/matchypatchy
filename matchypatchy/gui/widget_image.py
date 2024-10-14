d9650769f2f1dbac0c0fbb0fa74a45d9514bdc8c
Kyra Swanson <t.kyraswanson@gmail.com>
2024-10-14 15:08:59 -0700

    fixed image widget resize

--- matchypatchy/gui/widget_image.py
+++ matchypatchy/gui/widget_image.py
@@ -3,7 +3,7 @@ from PIL.ImageQt import ImageQt
 
 from PyQt6.QtWidgets import QLabel
 from PyQt6.QtGui import QPixmap, QPainter, QImage
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QRect
 
 
 class ImageWidget(QLabel):
@@ -14,85 +14,47 @@ class ImageWidget(QLabel):
         self.image_path = image_path
         
         # Create a QLabel to hold the image
-        self.image_label = QLabel(self)
-        self.image_label.setMinimumSize(self.default_width, self.default_height)
-        self.image_label.setScaledContents(True)
+        self.setMinimumSize(self.default_width,self.default_height)
+        self.setScaledContents(True)
         
-        self.pixmap = QPixmap(self.default_width,self.default_height)
+        self.pixmap = QPixmap(self.size())
         self.pixmap.fill(Qt.GlobalColor.black)
-        self.image_label.setPixmap(self.pixmap)
-
-        # Allow the QLabel to scale its content (the pixmap) with the widget's size
+        self.setPixmap(self.pixmap)
         
         
     def display_image(self, image_path, bbox=None):
         if image_path != self.image_path:
             self.image_path = image_path
-        self.image_label.clear()
-
-        # crop first
-        if bbox is not None:
-            image = QImage(image_path)
-            img_width = image.width()
-            img_height = image.height()
-            
-            left = img_width * bbox['bbox_x']
-            top = img_height * bbox['bbox_y']
-            right = img_width * (bbox['bbox_x'] + bbox['bbox_w'])
-            bottom = img_height * (bbox['bbox_y'] + bbox['bbox_h'])
-            crop_rect = (int(left), int(top), int(right), int(bottom))
-
-            print(crop_rect)
-            cropped_image = image.copy(*crop_rect)
-
-            self.pixmap = QPixmap.fromImage(cropped_image)
-            self.image_label.setPixmap(self.pixmap)
-        
-        # default load
-        else:
-            self.pixmap = QPixmap(self.image_path)  # Update the path to your image file
-            self.image_label.setPixmap(self.pixmap)
-
-        
-    def display_image_padded(self, image_path, bbox=None):
-        if image_path != self.image_path:
-            self.image_path = image_path
-        self.image_label.clear()
+        self.clear()
         self.pixmap = QPixmap(self.width(), self.height())
         self.pixmap.fill(Qt.GlobalColor.black)
-        self.image_label.setPixmap(self.pixmap)
-        # crop
+
+        self.original = QImage(image_path)
+
         if bbox is not None:
-            image = QImage(image_path)
-            img_width = image.width()
-            img_height = image.height()
-            
-            left = img_width * bbox['bbox_x']
-            top = img_height * bbox['bbox_y']
-            right = img_width * (bbox['bbox_x'] + bbox['bbox_w'])
-            bottom = img_height * (bbox['bbox_y'] + bbox['bbox_h'])
-            crop_rect = (int(left), int(top), int(right), int(bottom))
-
-            print(crop_rect)
-            cropped_image = image.copy(*crop_rect)
-
-            self.pixmap = QPixmap.fromImage(cropped_image)
+            left = self.original.width() * bbox['bbox_x']
+            top = self.original.height() * bbox['bbox_y']
+            right = self.original.width() * bbox['bbox_w']
+            bottom = self.original.height() * bbox['bbox_h']
+            crop_rect = QRect(int(left), int(top), int(right), int(bottom))
+            self.image = self.original.copy(crop_rect)
         
-        # default load
         else:
-            self.pixmap = QPixmap(self.image_path)
+            self.image = self.original.copy()
 
+        scaled_image = self.image.scaled(self.size(), 
+                                         Qt.AspectRatioMode.KeepAspectRatio, 
+                                         Qt.TransformationMode.SmoothTransformation)
         
-        self.image_label.setPixmap(self.pixmap)
+        painter = QPainter(self.pixmap)
+        x_offset = (self.width() - scaled_image.width()) // 2
+        y_offset = (self.height() - scaled_image.height()) // 2
+        painter.drawImage(x_offset, y_offset, scaled_image)
+        painter.end()
+
+        self.setPixmap(self.pixmap)
 
 
+    
     def resizeEvent(self, event):
-        # Get the current size of the widget
-        # Scale the pixmap to the size of the widget, keeping the aspect ratio
-        self.image_label.clear()
-        self.setPixmap(self.pixmap.scaled(self.width(), self.height(),
-                                          Qt.AspectRatioMode.KeepAspectRatio,
-                                          Qt.TransformationMode.SmoothTransformation))
-
-        # Call the parent class's resizeEvent
         super().resizeEvent(event)
