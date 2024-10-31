522e69e0c05b996b5b463ba6bfa9d89e5c48638c
tkswanson <ts9137b@gmail.com>
2024-10-30 19:09:09 -0700

    add new filters for ind, favorite, unidentified

--- matchypatchy/gui/display_media.py
+++ matchypatchy/gui/display_media.py
@@ -4,13 +4,10 @@ GUI Window for viewing images
 from PyQt6.QtWidgets import (QPushButton, QWidget, QVBoxLayout, QHBoxLayout,
                              QLabel, QComboBox)
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QKeyEvent
 
 from .media_table import MediaTable
 from .widget_progressbar import ProgressPopup
 
-# TODO:
-# MAKE MEDIA EDITABLE
 
 class DisplayMedia(QWidget):
     def __init__(self, parent):
@@ -31,10 +28,6 @@ class DisplayMedia(QWidget):
         survey_label = QLabel("Filter:")
         survey_label.setFixedWidth(50)
         first_layer.addWidget(survey_label, 0, alignment=Qt.AlignmentFlag.AlignLeft)
-
-        unidentified = QPushButton("Unidentified Only")
-        unidentified.clicked.connect(self.select_unidentified)
-        first_layer.addWidget(unidentified, 0, alignment=Qt.AlignmentFlag.AlignLeft)
 
         # SURVEY 
         self.survey_select = QComboBox()
@@ -61,24 +54,47 @@ class DisplayMedia(QWidget):
         self.species_select.addItems([el[1] for el in self.species_list_ordered])
         self.active_species = self.species_list_ordered[0]
 
+        # INDIVIDUAL 
+        self.individual_select = QComboBox()
+        self.individual_select.setFixedWidth(200)
+        first_layer.addWidget(self.individual_select, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.individual_list_ordered = [(0,'Individual')]
+        self.individual_select.addItems([el[1] for el in self.individual_list_ordered])
+        self.active_individual = self.individual_list_ordered[0]
+
+        # UNIDENTIFIED
+        unidentified = QPushButton("Unidentified")
+        unidentified.setCheckable(True)
+        unidentified.toggled.connect(self.select_unidentified) 
+        self.unidentified_only = False
+        first_layer.addWidget(unidentified, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+
+        # FAVORITE
+        favorites = QPushButton("Favorites")
+        favorites.setCheckable(True)
+        favorites.toggled.connect(self.select_favorites) 
+        self.favorites_only = False
+        first_layer.addWidget(favorites, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+
         first_layer.addStretch()
         layout.addLayout(first_layer)
 
-        # MEDIA TABLE
+        # display rois or media
         self.media_table = MediaTable(self)
         layout.addWidget(self.media_table, stretch=1)
         self.setLayout(layout)
 
-
+    # Return Home Button
     def home(self):
         self.parent._set_base_view()
 
+    # 1. RUN ON ENTRY
     def refresh_filters(self):
         """
         Update Dropdown Lists
         """
         self.survey_select.clear()
-        self.survey_list_ordered = [(0,'Survey')] + list(self.mpDB.select('survey',columns='id, name'))
+        self.survey_list_ordered = [(0,'Survey')] + list(self.mpDB.select('survey', columns='id, name'))
         self.survey_select.addItems([el[1] for el in self.survey_list_ordered])
         self.active_survey = self.survey_list_ordered[self.survey_select.currentIndex()]
 
@@ -89,38 +105,58 @@ class DisplayMedia(QWidget):
         self.active_site = self.site_list_ordered[self.site_select.currentIndex()]
 
         self.species_select.clear()
-        self.species_list_ordered = [(0,'Species')] + list(self.mpDB.select('species',columns='id, common'))
+        self.species_list_ordered = [(0,'Species')] + list(self.mpDB.select('species', columns='id, common'))
         self.species_select.addItems([el[1] for el in self.species_list_ordered])
         self.active_species = self.species_list_ordered[self.species_select.currentIndex()]
 
+        self.individual_select.clear()
+        self.individual_list_ordered = [(0,'Individual')] + list(self.mpDB.select('individual', columns='id, name'))
+        self.individual_select.addItems([el[1] for el in self.individual_list_ordered])
+        self.active_individual = self.individual_list_ordered[self.individual_select.currentIndex()]
+
+    # 2. RUN ON ENTRY
     def connect(self):
         self.survey_select.currentIndexChanged.connect(self.select_survey)
         self.site_select.currentIndexChanged.connect(self.select_site)
         self.species_select.currentIndexChanged.connect(self.select_species)
+        self.individual_select.currentIndexChanged.connect(self.select_individual)
 
+    # 3. RUN ON ENTRY
     def update_table(self):
-        # TODO: load images only once, store in temp
         self.loading_bar = ProgressPopup(self, "Loading images...")
-        self.loading_bar.show()
-        self.media_table.update()
+        self.media_table.load()
 
-    def select_unidentified():
-        pass
+    def filter_table(self):
+        """
+        Filter the media table based on the selected options
+        Run after any setting is changed
+        """
+        self.media_table.filter()
+
+    def select_unidentified(self):
+        self.unidentified_only = not self.unidentified_only
+        self.filter_table() 
+
+    def select_favorites(self):
+        self.favorites_only = not self.favorites_only
+        self.filter_table()    
 
     def select_survey(self):
-        print('survey changed')
         self.active_survey = self.survey_list_ordered[self.survey_select.currentIndex()]
-        self.update_table()
+        self.filter_sites()
+        self.filter_table()
 
     def select_site(self):
-        print('site changed')
         self.active_site = self.site_list_ordered[self.site_select.currentIndex()]
-        self.update_table()
+        self.filter_table()
         
     def select_species(self):
-        print('species changed')
         self.active_species = self.species_list_ordered[self.species_select.currentIndex()]
-        self.update_table()
+        self.filter_table()
+
+    def select_individual(self):
+        self.active_individual = self.individual_list_ordered[self.individual_select.currentIndex()]
+        self.filter_table()
 
     def filter_sites(self):
         # Update site list to reflect active survey
@@ -131,12 +167,4 @@ class DisplayMedia(QWidget):
             self.valid_sites = dict(self.mpDB.select("site", columns="id, name"))
         self.site_list_ordered = [(0,'Site')] + [(k, v) for k, v in self.valid_sites.items()]
         self.site_select.addItems([el[1] for el in self.site_list_ordered])
-
-    # Keyboard Handler
-    def keyPressEvent(self, event):
-        key = event.key()
-        key_text = event.text()
-        print(f"Key pressed: {key_text} (Qt key code: {key})")
-
-
 
