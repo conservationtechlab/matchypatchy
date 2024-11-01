98d0f21947ab24d99b0fa01041798bee7db0df8a
Kyra Swanson <t.kyraswanson@gmail.com>
2024-11-01 12:48:30 -0700

    reorganize base layout

--- matchypatchy/gui/display_base.py
+++ matchypatchy/gui/display_base.py
@@ -33,87 +33,98 @@ class DisplayBase(QWidget):
         
         layout = QVBoxLayout()
 
-        self.label = QLabel("Welcome to MatchyPatchy")
-        self.label.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.label = QLabel("Welcome To MatchyPatchy")
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.label.setFixedHeight(20)
         layout.addWidget(self.label)
-        layout.addStretch()
+        layout.addSpacing(300)
 
-        # Surveys
-        first_layer = QHBoxLayout()
+        column_layout = QHBoxLayout()
+        column_layout.addSpacing(100) # add spacing to left side
+
+        # DB MANAGEMENT
+        db_layer = QVBoxLayout()
+        db_layer.addWidget(QLabel("Database Management"),
+                           alignment=Qt.AlignmentFlag.AlignCenter)
+
+        #Survey
+        survey_layout = QHBoxLayout()
         survey_label = QLabel("Survey:")
-        first_layer.addWidget(survey_label, 0)
+        survey_layout.addWidget(survey_label,0)
         self.survey_select = QComboBox()
         self.survey_select.currentIndexChanged.connect(self.select_survey)
-        first_layer.addWidget(self.survey_select, 1)
-
-        button_survey_new = QPushButton("New Survey")
+        survey_layout.addWidget(self.survey_select,1)
+        button_survey_new = QPushButton("+")
         button_survey_new.clicked.connect(self.new_survey)
-        first_layer.addWidget(button_survey_new, 1)
-
+        survey_layout.addWidget(button_survey_new,0)
+        db_layer.addLayout(survey_layout,0)
+        
         # Sites
         self.button_site_manage = QPushButton("Manage Sites")
         self.button_site_manage.clicked.connect(self.new_site)
-        first_layer.addWidget(self.button_site_manage, 1)
+        db_layer.addWidget(self.button_site_manage)
         self.button_manage_site_flag = False
         self.button_site_manage.setEnabled(self.button_manage_site_flag)
 
         self.button_species_manage = QPushButton("Manage Species")
         self.button_species_manage.clicked.connect(self.manage_species)
-        first_layer.addWidget(self.button_species_manage, 1)
+        db_layer.addWidget(self.button_species_manage)
 
         self.button_media_manage = QPushButton("Manage Media")
         self.button_media_manage.clicked.connect(self.manage_media)
-        first_layer.addWidget(self.button_media_manage, 1)
+        db_layer.addWidget(self.button_media_manage)
 
-        self.update_survey()
-        layout.addLayout(first_layer)
-        layout.addSpacing(50)
+        db_layer.addStretch()
+        column_layout.addLayout(db_layer,1)
+        column_layout.addSpacing(20)
 
+        # IMPORT
+        import_layer = QVBoxLayout()
+        import_layer.addWidget(QLabel("Import and Process Data"),
+                               alignment=Qt.AlignmentFlag.AlignCenter)
 
-        option1 = QLabel("Option 1:")
-        option1.setMaximumHeight(20)
-        layout.addWidget(option1)
-
-        # Bottom Layer
-        csv_layer = QHBoxLayout()
-        # Create three buttons
-        button_csv_load = QPushButton("1. Import from CSV")
-        button_csv_process = QPushButton("2. Process")
-        button_csv_match = QPushButton("3. Match")
         
-        button_csv_load.clicked.connect(self.import_csv)
-        button_csv_process.clicked.connect(self.process_images)
-        button_csv_match.clicked.connect(self.match)
+        button_load_csv = QPushButton("1. Import from CSV")
+        button_load_folder = QPushButton("1. Import from Folder")
+        button_process = QPushButton("2. Process")
+        button_validate = QPushButton("3. Validate")
+        button_match = QPushButton("4. Match")
+        
+        button_load_csv.clicked.connect(self.import_csv)
+        button_load_folder.clicked.connect(self.import_folder)
+        button_process.clicked.connect(self.process_images)
+        button_validate.clicked.connect(self.validate)
+        button_match.clicked.connect(self.match)
 
         # Add buttons to the layout
-        csv_layer.addWidget(button_csv_load)
-        csv_layer.addWidget(button_csv_process)
-        csv_layer.addWidget(button_csv_match)
-        layout.addLayout(csv_layer) 
+        load_layout = QHBoxLayout()
+        load_layout.addWidget(button_load_csv)
+        load_layout.addWidget(button_load_folder)
+        import_layer.addLayout(load_layout)
+        import_layer.addWidget(button_process)
+        import_layer.addWidget(button_validate)
+        import_layer.addWidget(button_match)
 
-        option2 = QLabel("Option 2:")
-        option1.setMaximumHeight(20)
-        layout.addWidget(option2)
-
-        folder_layer = QHBoxLayout()
-
-        button_folder_load = QPushButton("1. Import from Folder")
-        button_folder_process = QPushButton("2. Process")
-        button_folder_validate = QPushButton("3. Validate")
-        button_folder_match = QPushButton("4. Match")
-
-        button_folder_load.clicked.connect(self.import_folder)
-        button_folder_process.clicked.connect(self.process_images)
-        button_folder_validate.clicked.connect(self.validate)
-        button_folder_match.clicked.connect(self.match)
-
-        folder_layer.addWidget(button_folder_load)
-        folder_layer.addWidget(button_folder_process)
-        folder_layer.addWidget(button_folder_validate)
-        folder_layer.addWidget(button_folder_match)
-        layout.addLayout(folder_layer) 
+        import_layer.addStretch()
+        column_layout.addLayout(import_layer,1) 
+        column_layout.addSpacing(20)
         
+        # OTHER
+        other_layer = QVBoxLayout()
+        other_layer.addWidget(QLabel("Other Options"),
+                              alignment=Qt.AlignmentFlag.AlignCenter)
+
+        button_download_ml = QPushButton("Download Models")
+        other_layer.addWidget(button_download_ml)
+
+        other_layer.addStretch()
+        column_layout.addLayout(other_layer,1) 
+        column_layout.addSpacing(100) # add spacing to right side
+
+        layout.addLayout(column_layout)
         self.setLayout(layout)
+
+        self.update_survey()
 
     def new_survey(self):
         dialog = SurveyFillPopup(self)
