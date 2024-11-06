52f2da59922bd2a5f878e01d9d00bc84c2c91bab
Kyra Swanson <t.kyraswanson@gmail.com>
2024-11-05 16:49:59 -0800

    add downloadable models

--- matchypatchy/gui/popup_downloadml.py
+++ matchypatchy/gui/popup_downloadml.py
@@ -1,53 +1,86 @@
 """
 Popup for Selection within a list, ie Survey selection
+
+0 MegaDetector v5a
+1 Andes
+2 Amazon
+3 Savanna
+4 SE Asia Rainforest
+5 Southwest USA
+
 """
-from PyQt6.QtWidgets import QDialog, QVBoxLayout, QGridLayout, QPushButton, QCheckBox, QLabel
+import os
+import wget
+from PyQt6.QtWidgets import QDialog, QVBoxLayout, QGridLayout, QProgressBar, QCheckBox, QLabel, QDialogButtonBox
+from PyQt6.QtCore import Qt, QThread, pyqtSignal
+
+from matchypatchy import config
+from matchypatchy.ml import models
+
+
 
 class MLDownloadPopup(QDialog):
-    def __init__(self, parent, table, column):
+    def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-        self.table = table
-        self.column = column
-        self.selected_option = None
+        self.checked_models = set()
 
         self.setWindowTitle("Select Models")
 
         # Create layout for the checkboxes and add it to the dialog
         layout = QVBoxLayout(self)
-        grid_layout = QGridLayout()
-        layout.addLayout(grid_layout)
-
-        self.items = ["MegaDetector", "Southwest", "Andes", "Amazon"]
+        self.checkbox_layout = QGridLayout()
+        layout.addLayout(self.checkbox_layout)
 
         # Add checkboxes to the grid layout in two columns
-        for index, item in enumerate(self.items):
-            checkbox = QCheckBox(item)
-            row = index // 2  # Determine the row based on index
-            col = index % 2   # Alternate between column 0 and column 1
-            grid_layout.addWidget(checkbox, row, col)
+        for m in models.MODELS:
+            checkbox = QCheckBox(models.MODELS[m][0])
+            self.checkbox_layout.addWidget(checkbox, m, 0)
 
         # Add OK and Cancel buttons
-        button_ok = QPushButton("OK")
-        button_ok.clicked.connect(self.accept)
-        button_cancel = QPushButton("Cancel")
-        button_cancel.clicked.connect(self.reject)
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok|QDialogButtonBox.StandardButton.Cancel)
+        layout.addWidget(self.buttonBox, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.buttonBox.accepted.connect(self.accept)  
+        self.buttonBox.rejected.connect(self.reject)
 
-        layout.addWidget(button_ok)
-        layout.addWidget(button_cancel)
+          # Progress Bar (hidden at start)
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setTextVisible(False)
+        self.progress_bar.hide()
+        layout.addWidget(self.progress_bar)
+
+        self.available_models = models.import_models()
+        self.set_checkboxes()
+        
+
+    def set_checkboxes(self):
+        for m in self.available_models.keys():
+            checkbox = self.checkbox_layout.itemAtPosition(m,0).widget()
+            checkbox.setChecked(True)
+
+    def toggle_checkbox(self):
+        pass
+    
+    def download_ml(self):
+        self.progress_bar.setRange(0,len(self.checked_models))
+        self.progress_bar.show()
+        self.build_thread = DownloadMLThread(self.checked_models)
+        self.build_thread.downloaded.connect(self.progress_bar.setValue)
+        self.build_thread.start()
 
 
-    def select(self):
-        try:
-            self.selected_option = self.list[self.combo.currentIndex()]
-            return True
-        except IndexError:
-            return False
+class DownloadMLThread(QThread):
+    """
+    Thread for launching 
+    """
+    downloaded = pyqtSignal(int)
+   
 
-    def get_options(self):
-        options = self.parent.mpDB.select(self.table, columns = f"id, {self.column}")
-        self.list = list(options)
-        self.combo.addItems([el[1] for el in self.list])
+    def __init__(self, checked_models):
+        super().__init__()
+        self.checked_models = checked_models
 
-    def get_selection(self):
-        return self.selected_option
+    def run(self):
+        for model in self.checked_models:
+            wget.download(models.MODELS[model][2], out=config.ML_PATH)
+            self.downloaded.emit(model)
--- matchypatchy/ml/models.py
+++ matchypatchy/ml/models.py
@@ -2,4 +2,56 @@
 Functions for managing ML models
 
 """
-import matchypatchy.config
+import wget
+from pathlib import Path
+from matchypatchy import config
+
+MODELS = {0:("MegaDetector v5a", "md_v5a.0.0.pt", ), 2:("Andes", "sdzwa_andes_v1.pt"),
+          1:("MegaDetector v5b", "md_v5b.0.0.pt", ), 3:("Amazon", "sdzwa_amazon_v1.onnx"),
+          7:("MiewID v3", "miewid_v3.bin"),          4:("Savanna", "sdzwa_savanna_v3.pt"), 
+          8:("Viewpoint", "viewpoint_jaguar.pt"),    5:("SE Asian Rainforest", "sdzwa_seasia_v1.pt"), 
+                                                     6:("Southwest USA", "sdzwa_southwest_v3.pt"),
+         }
+
+CLASS_FILES = {2:("Andes", "sdzwa_andes_v1.csv"),
+               3:("Amazon", "sdzwa_amazon_v1.csv"),
+               4:("Savanna", "sdzwa_savanna_v3.csv"),
+               5:("SE Asian Rainforest", "sdzwa_seasia_v1.csv"),
+               6:("Southwest USA", "sdzwa_southwest_v3.csv"),}
+
+MEGADETECTOR_DEFAULT = [0]
+MIEW_DEFAULT = [7]
+
+DETECTORS = [0,1]
+CLASSIFIERS = [2,3,4,5,6]
+
+def import_models(keys=MODELS.keys()):
+    available_models = dict()
+    for m in keys:
+        path = config.ML_DIR / MODELS[m][1]
+        if path.exists():
+            available_models[m] = path
+
+    if len(available_models == 1):
+        return list(available_models)
+    return available_models
+
+def get_path(key):
+    path = config.ML_DIR / MODELS[key][1]
+    if path.exists():
+        return path
+    else:
+        return None
+    
+def get_class_path(key):
+    path = config.ML_DIR / CLASS_FILES[key][1]
+    if path.exists():
+        return path
+    else:
+        return None
+    
+def download(key):
+    url = MODELS[key][2]
+    path = config.ML_DIR / MODELS[key][1]
+    wget.download(url,out=path)
+        
