55666e1f99ddaa69fd88b875eb7a1750879c9cd4
Kyra Swanson <t.kyraswanson@gmail.com>
2024-11-07 13:47:50 -0800

    use name keys for models

--- matchypatchy/ml/models.py
+++ matchypatchy/ml/models.py
@@ -6,37 +6,36 @@ import wget
 from pathlib import Path
 from matchypatchy import config
 
-MODELS = {0:("MegaDetector v5a", "md_v5a.0.0.pt", "https://sandiegozoo.box.com/shared/static/xj3496ii5hxtomf0s38axb1agn5u9up8.pt"), 
-          1:("MegaDetector v5b", "md_v5b.0.0.pt", ),
-          2:("Andes", "sdzwa_andes_v1.pt"),
-          3:("Amazon", "sdzwa_amazon_v1.onnx"),
-          4:("Savanna", "sdzwa_savanna_v3.pt"), 
-          5:("SE Asian Rainforest", "sdzwa_seasia_v1.pt"), 
-          6:("Southwest USA", "sdzwa_southwest_v3.pt", 'https://sandiegozoo.box.com/shared/static/ucbk8kc2h3qu15g4xbg0nvbghvo1cl97.pt'),
-          7:("MiewID v2", "miewid_v2.bin"), 
-          8:("MiewID v3", "miewid_v3.bin"),          
-          9:("Viewpoint", "viewpoint_jaguar.pt"),    
-                                                     
+MODELS = {"MegaDetector v5a": ("md_v5a.0.0.pt", "https://sandiegozoo.box.com/shared/static/xj3496ii5hxtomf0s38axb1agn5u9up8.pt"), 
+          "MegaDetector v5b": ("md_v5b.0.0.pt", ),
+          "Andes": ("sdzwa_andes_v1.pt"),
+          "Amazon Rainforest": ("sdzwa_amazon_v1.onnx",),
+          "Savanna":  ("sdzwa_savanna_v3.pt",), 
+          "SE Asian Rainforest": ("sdzwa_seasia_v1.pt",), 
+          "Southwest USA": ("sdzwa_southwest_v3.pt", 'https://sandiegozoo.box.com/shared/static/ucbk8kc2h3qu15g4xbg0nvbghvo1cl97.pt'),
+          "MiewID v2": ("miewid_v2.bin",), 
+          "MiewID v3": ("miewid_v3.bin",),          
+          "Jaguar Viewpoint": ("viewpoint_jaguar.pt",),                        
          }
 
-CLASS_FILES = {2:("Andes", "sdzwa_andes_v1.csv"),
-               3:("Amazon", "sdzwa_amazon_v1.csv"),
-               4:("Savanna", "sdzwa_savanna_v3.csv"),
-               5:("SE Asian Rainforest", "sdzwa_seasia_v1.csv"),
-               6:("Southwest USA", "sdzwa_southwest_v3_classes.csv", 'https://sandiegozoo.box.com/shared/static/tetfkotf295espoaw8jyco4tk1t0trtt.csv'),}
+CLASS_FILES = {"Andes": ("sdzwa_andes_v1_classes.csv"),
+               "Amazon Rainforest": ("sdzwa_amazon_v1_classes.csv"),
+               "Savanna": ("sdzwa_savanna_v3_classes.csv"),
+               "SE Asian Rainforest": ("sdzwa_seasia_v1_classes.csv"),
+               "Southwest USA": ("sdzwa_southwest_v3_classes.csv", 'https://sandiegozoo.box.com/shared/static/tetfkotf295espoaw8jyco4tk1t0trtt.csv'),}
 
 MEGADETECTOR_DEFAULT = [0]
 MIEW_DEFAULT = [7]
 
-DETECTORS = [0,1]
-CLASSIFIERS = [2,3,4,5,6]
-REIDS = [7,8]
-VIEWPOINTS = [9]
+DETECTORS = ["MegaDetector v5a", "MegaDetector v5b"]
+CLASSIFIERS = ["Andes","Amazon Rainforest", "Savanna", "SE Asian Rainforest", "Southwest USA"]
+REIDS = ["MiewID v2", "MiewID v3"]
+VIEWPOINTS = ["Jaguar Viewpoint"]
 
 def available_models(keys=MODELS.keys()):
     models_dict = dict()
     for m in keys:
-        path = config.ML_DIR / MODELS[m][1]
+        path = config.ML_DIR / MODELS[m][0]
         if path.exists():
             models_dict[m] = path
     # if looking for a particular model, give back path
@@ -45,7 +44,7 @@ def available_models(keys=MODELS.keys()):
 def get_path(key):
     if key is None: 
         return None
-    path = config.ML_DIR / MODELS[key][1]
+    path = config.ML_DIR / MODELS[key][0]
     if path.exists():
         return path
     else:
@@ -54,14 +53,14 @@ def get_path(key):
 def get_class_path(key):
     if key is None: 
         return None
-    path = config.ML_DIR / CLASS_FILES[key][1]
+    path = config.ML_DIR / CLASS_FILES[key][0]
     if path.exists():
         return path
     else:
         return None
     
 def download(key):
-    url = MODELS[key][2]
-    path = config.ML_DIR / MODELS[key][1]
+    url = MODELS[key][1]
+    path = config.ML_DIR / MODELS[key][0]
     wget.download(url,out=path)
         
