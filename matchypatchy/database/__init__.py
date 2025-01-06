8d963ff113bc65254ae5b9fe72026279693abbc1
Kyra Swanson <t.kyraswanson@gmail.com>
2025-01-06 10:07:00 -0800

    update init files

--- matchypatchy/database/__init__.py
+++ matchypatchy/database/__init__.py
@@ -1,4 +1,3 @@
-from matchypatchy.database import individual
 from matchypatchy.database import media
 from matchypatchy.database import mpdb
 from matchypatchy.database import roi
@@ -7,8 +6,8 @@ from matchypatchy.database import site
 from matchypatchy.database import species
 from matchypatchy.database import survey
 
-from matchypatchy.database.individual import (merge,)
-from matchypatchy.database.media import (IMAGE_EXT, VIDEO_EXT, fetch_media,)
+from matchypatchy.database.media import (COLUMNS, IMAGE_EXT, VIDEO_EXT,
+                                         fetch_media,)
 from matchypatchy.database.mpdb import (MatchyPatchyDB,)
 from matchypatchy.database.roi import (fetch_roi, fetch_roi_media, get_bbox,
                                        get_sequence, roi_metadata,
@@ -16,11 +15,11 @@ from matchypatchy.database.roi import (fetch_roi, fetch_roi_media, get_bbox,
 from matchypatchy.database.setup import (setup_database,)
 from matchypatchy.database.site import (fetch_sites, import_csv,)
 from matchypatchy.database.species import (fetch_species, import_csv,)
-from matchypatchy.database.survey import (fetch_surveys,)
+from matchypatchy.database.survey import (fetch_regions, fetch_surveys,)
 
-__all__ = ['IMAGE_EXT', 'MatchyPatchyDB', 'VIDEO_EXT', 'fetch_media',
-           'fetch_roi', 'fetch_roi_media', 'fetch_sites', 'fetch_species',
-           'fetch_surveys', 'get_bbox', 'get_sequence', 'import_csv',
-           'individual', 'media', 'merge', 'mpdb', 'roi', 'roi_metadata',
-           'sequence_roi_dict', 'setup', 'setup_database', 'site', 'species',
-           'survey']
+__all__ = ['COLUMNS', 'IMAGE_EXT', 'MatchyPatchyDB', 'VIDEO_EXT',
+           'fetch_media', 'fetch_regions', 'fetch_roi', 'fetch_roi_media',
+           'fetch_sites', 'fetch_species', 'fetch_surveys', 'get_bbox',
+           'get_sequence', 'import_csv', 'media', 'mpdb', 'roi',
+           'roi_metadata', 'sequence_roi_dict', 'setup', 'setup_database',
+           'site', 'species', 'survey']
--- matchypatchy/gui/__init__.py
+++ matchypatchy/gui/__init__.py
@@ -5,11 +5,13 @@ from matchypatchy.gui import display_single
 from matchypatchy.gui import main_gui
 from matchypatchy.gui import media_table
 from matchypatchy.gui import popup_alert
+from matchypatchy.gui import popup_config
 from matchypatchy.gui import popup_dropdown
 from matchypatchy.gui import popup_import_csv
 from matchypatchy.gui import popup_import_folder
 from matchypatchy.gui import popup_individual
 from matchypatchy.gui import popup_ml
+from matchypatchy.gui import popup_single_image
 from matchypatchy.gui import popup_site
 from matchypatchy.gui import popup_species
 from matchypatchy.gui import popup_survey
@@ -23,31 +25,35 @@ from matchypatchy.gui.main_gui import (MainWindow, main_display,)
 from matchypatchy.gui.media_table import (LoadThumbnailThread, MediaTable,
                                           THUMBNAIL_NOTFOUND,)
 from matchypatchy.gui.popup_alert import (AlertPopup, ProgressPopup,)
+from matchypatchy.gui.popup_config import (ConfigPopup,)
 from matchypatchy.gui.popup_dropdown import (DropdownPopup,)
 from matchypatchy.gui.popup_import_csv import (CSVImportThread,
                                                ImportCSVPopup,)
 from matchypatchy.gui.popup_import_folder import (BuildManifestThread,
                                                   FolderImportThread,
                                                   ImportFolderPopup,)
-from matchypatchy.gui.popup_individual import (IndividualFillPopup,)
+from matchypatchy.gui.popup_individual import (IndividualFillPopup,
+                                               IndividualPopup,)
 from matchypatchy.gui.popup_ml import (DownloadMLThread, MLDownloadPopup,
                                        MLOptionsPopup,)
+from matchypatchy.gui.popup_single_image import (ImagePopup,)
 from matchypatchy.gui.popup_site import (SiteFillPopup, SitePopup,)
 from matchypatchy.gui.popup_species import (SpeciesFillPopup, SpeciesPopup,)
 from matchypatchy.gui.popup_survey import (SurveyFillPopup, SurveyPopup,)
 from matchypatchy.gui.widget_image import (ImageWidget,)
 
 __all__ = ['AlertPopup', 'BuildManifestThread', 'CSVImportThread',
-           'DisplayBase', 'DisplayCompare', 'DisplayMedia', 'DisplaySingle',
-           'DownloadMLThread', 'DropdownPopup', 'FolderImportThread',
-           'ImageWidget', 'ImportCSVPopup', 'ImportFolderPopup',
-           'IndividualFillPopup', 'LoadThumbnailThread', 'MATCH_STYLE',
-           'MLDownloadPopup', 'MLOptionsPopup', 'MainWindow', 'MediaTable',
-           'ProgressPopup', 'SiteFillPopup', 'SitePopup', 'SpeciesFillPopup',
-           'SpeciesPopup', 'SurveyFillPopup', 'SurveyPopup',
-           'THUMBNAIL_NOTFOUND', 'TableEditorPopup', 'display_base',
-           'display_compare', 'display_media', 'display_single',
-           'main_display', 'main_gui', 'media_table', 'popup_alert',
-           'popup_dropdown', 'popup_import_csv', 'popup_import_folder',
-           'popup_individual', 'popup_ml', 'popup_site', 'popup_species',
-           'popup_survey', 'popup_table', 'widget_image']
+           'ConfigPopup', 'DisplayBase', 'DisplayCompare', 'DisplayMedia',
+           'DisplaySingle', 'DownloadMLThread', 'DropdownPopup',
+           'FolderImportThread', 'ImagePopup', 'ImageWidget', 'ImportCSVPopup',
+           'ImportFolderPopup', 'IndividualFillPopup', 'IndividualPopup',
+           'LoadThumbnailThread', 'MATCH_STYLE', 'MLDownloadPopup',
+           'MLOptionsPopup', 'MainWindow', 'MediaTable', 'ProgressPopup',
+           'SiteFillPopup', 'SitePopup', 'SpeciesFillPopup', 'SpeciesPopup',
+           'SurveyFillPopup', 'SurveyPopup', 'THUMBNAIL_NOTFOUND',
+           'display_base', 'display_compare', 'display_media',
+           'display_single', 'main_display', 'main_gui', 'media_table',
+           'popup_alert', 'popup_config', 'popup_dropdown', 'popup_import_csv',
+           'popup_import_folder', 'popup_individual', 'popup_ml',
+           'popup_single_image', 'popup_site', 'popup_species', 'popup_survey',
+           'widget_image']
