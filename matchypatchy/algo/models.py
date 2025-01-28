23676760c88ee6f0fe28c2ef1d81b1b2dbfdc547
Kyra Swanson <t.kyraswanson@gmail.com>
2025-01-28 14:55:43 -0800

    fix model download

--- matchypatchy/algo/models.py
+++ matchypatchy/algo/models.py
@@ -66,16 +66,21 @@ def download(key):
     # read model directory
     ML_DIR = Path(config.load("ML_DIR"))
     
-    with open('models.yml', 'r') as ml_cfg:
     
-        path = ML_DIR / ml_cfg['MODELS'][key][0]
+    with open('models.yml', 'r') as cfg_file:
+        ml_cfg = yaml.safe_load(cfg_file)
+        models = ml_cfg['MODELS']        
+        name = models[key][0]
+        url = models[key][1]
+    
+        path = ML_DIR / Path(name)
 
         if not path.exists():  # check to see if it already exists first
             urllib.request.urlretrieve(ml_cfg['MODELS'][key][1], path)
 
         if path.exists():  # validate that it downloaded
             # if key is a classifier, get class list and config
-            if key in ml_cfg['CLASSIFIERS']: 
+            if key in ml_cfg['CLASSIFIER_MODELS']: 
                 class_path = ML_DIR / ml_cfg['CLASS_FILES'][key][0]
                 config_path = ML_DIR / ml_cfg['CONFIG_FILES'][key][0]
                 if not class_path.exists():
