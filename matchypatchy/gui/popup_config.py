ed3f1b9d62e0b9c18bd434142172c6287db31990
Kyra Swanson <t.kyraswanson@gmail.com>
2025-06-02 10:49:57 -0700

    add direct db command

--- matchypatchy/gui/popup_config.py
+++ matchypatchy/gui/popup_config.py
@@ -3,6 +3,7 @@ Popup to add or edit species
 '''
 import os
 import logging
+from pathlib import Path
 
 from PyQt6.QtWidgets import (QDialog, QVBoxLayout, QHBoxLayout, QFileDialog,
                              QPushButton, QLineEdit, QLabel, QDialogButtonBox)
@@ -27,36 +28,59 @@ class ConfigPopup(QDialog):
 
         layout = QVBoxLayout()
 
-        # Paths
+        # TITLES ---------------------------------------------------------------
         path_layout = QHBoxLayout()
 
         titles_layout = QVBoxLayout()
         titles_layout.addWidget(QLabel("Project Directory:"))
-        #titles_layout.addWidget(QLabel("Log File:"))
         #titles_layout.addWidget(QLabel("Model Directory:"))
+
+        # Advanced
+        self.advanced_command = QLabel("Database Command:")
+        self.advanced_command.hide()
+        titles_layout.addWidget(self.advanced_command)
         path_layout.addLayout(titles_layout)
         
+        # TEXT -----------------------------------------------------------------
         insert_layout = QVBoxLayout()
         self.home_dir = QLineEdit()
         self.home_dir.setText(str(self.cfg['HOME_DIR']))
         insert_layout.addWidget(self.home_dir)
 
+        # Advanced
+        self.command_line = QLineEdit()
+        self.command_line.setText("Enter SQL Command")
+        self.command_line.hide()
+        insert_layout.addWidget(self.command_line)
         path_layout.addLayout(insert_layout)
 
+        # BUTTONS --------------------------------------------------------------
         path_button_layout = QVBoxLayout()
-
         button_home_dir = QPushButton()
         button_home_dir.setMaximumHeight(30)
+        button_home_dir.setFixedWidth(30)
         button_home_dir.setIcon(QIcon(ICON_PENCIL))
         button_home_dir.clicked.connect(self.set_home_dir)
-
         path_button_layout.addWidget(button_home_dir)
+
+        # Advanced
+        self.button_command = QPushButton("↵")
+        self.button_command.setMaximumHeight(30)
+        self.button_command.setFixedWidth(30)
+        self.button_command.clicked.connect(self.command)
+        self.button_command.hide()
+        path_button_layout.addWidget(self.button_command)
         path_layout.addLayout(path_button_layout)
 
         layout.addLayout(path_layout)
 
         # Buttons
         button_layout = QHBoxLayout()
+
+        button_advanced = QPushButton("Advanced")
+        button_advanced.clicked.connect(self.show_advanced)
+        button_layout.addWidget(button_advanced)
+
         # Ok/Cancel Buttons
         buttonBox = QDialogButtonBox(QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel)
         button_layout.addWidget(buttonBox)
@@ -66,29 +90,35 @@ class ConfigPopup(QDialog):
 
         self.setLayout(layout)
 
-    def command(self, command):
-        self.mpDB._command(self)
+    def show_advanced(self):
+        self.advanced_command.show()
+        self.command_line.show()
+        self.button_command.show()
+
+    def command(self):
+        new_cmd = self.command_line.text()
+        self.mpDB._command(new_cmd)
 
     def set_home_dir(self):
-        new_db = QFileDialog.getExistingDirectory(self, "Get Project Folder",
+        new_project = QFileDialog.getExistingDirectory(self, "Get Project Folder",
                                                   os.path.expanduser('~'),)
-        
+        if new_project:
+            self.home_dir.setText(new_project)
+            new_db = Path(new_project) / "Database"
+            valid = self.mpDB.update_paths(new_db)
 
-        # TODO
-        # if new_db:
-        #     self.home_dir.setText(new_db)
-        #     valid = self.mpDB.update_paths(new_db)
-        #     if valid:
-        #     # Update config
-        #         self.cfg['DB_DIR'] = str(new_db)
-        #         config.update(self.cfg)
-        #         # Log changes
-        #         logging.info("DB_DIR CHANGED")
-        #         logging.info('DB_DIR: ' + self.cfg['DB_DIR'])
-        #     else:
-        #         dialog = AlertPopup(self, prompt="Database is invalid. Please select another path or delete.")
-        #         if dialog.exec():
-        #             del dialog
+
+            if valid:
+            # Update config
+                self.cfg['DB_DIR'] = str(new_db)
+                config.update(self.cfg)
+                # Log changes
+                logging.info("DB_DIR CHANGED")
+                logging.info('DB_DIR: ' + self.cfg['DB_DIR'])
+            else:
+                dialog = AlertPopup(self, prompt="Database is invalid. Please select another path or delete.")
+                if dialog.exec():
+                    del dialog
 
             # Update config
             # self.cfg['LOG_PATH'] = str(new_log)
