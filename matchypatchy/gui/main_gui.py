047f02ca6e83221c5c53750bf565475bfae1b024
Kyra Swanson <t.kyraswanson@gmail.com>
2025-12-26 16:18:36 -0800

    adjust base size for smaller screans

--- matchypatchy/gui/main_gui.py
+++ matchypatchy/gui/main_gui.py
@@ -34,10 +34,11 @@ class MainWindow(QMainWindow):
         self.mpDB = mpDB
         self.setWindowTitle("MatchyPatchy")
         screen_resolution = QGuiApplication.primaryScreen().geometry()
-        self.setMinimumSize(1200, 900)
-        # resize to full screen if small screen
-        if screen_resolution.width() < 1600:
-            self.resize(screen_resolution.width(), screen_resolution.height())
+        # print(screen_resolution)
+        default_width = min(1200, screen_resolution.width()*0.8)
+        default_height = min(900, screen_resolution.height()*0.8)
+        self.setGeometry(0, 0, default_width, default_height)
+        #self.setMinimumSize(1200, 900)
 
         # Create Menu Bar
         self._createMenuBar()
