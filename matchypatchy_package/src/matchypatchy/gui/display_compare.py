bd93e864300575f2efddd20680826c463ab62ebc
tkswanson <t.kyraswanson@gmail.com>
2026-06-30 16:50:53 -0700

    update match number in center

--- matchypatchy_package/src/matchypatchy/gui/display_compare.py
+++ matchypatchy_package/src/matchypatchy/gui/display_compare.py
@@ -470,7 +470,7 @@ class DisplayCompare(QWidget):
 
         self.match_n.setText("/ " + str(len(self.QueryContainer.current_match_rois)))
         self.match_number.setText(str(self.QueryContainer.current_match + 1))
-        self.match_counter.setText(str(self.QueryContainer.current_match + 1) + "/ " + str(len(self.QueryContainer.current_match_rois)))
+        self.match_counter.setText(str(self.QueryContainer.current_match + 1) + " / " + str(len(self.QueryContainer.current_match_rois)))
 
         self.query_image_bar.reset()
         self.match_image_bar.reset()
@@ -489,6 +489,7 @@ class DisplayCompare(QWidget):
         self.QueryContainer.set_match(n)
         self.match_image_bar.reset()
         self.match_number.setText(str(self.QueryContainer.current_match + 1))
+        self.match_counter.setText(str(self.QueryContainer.current_match + 1) + " / " + str(len(self.QueryContainer.current_match_rois)))
         self.load_match()
 
     def load_query(self):
